c25ec1e7f0b37aa1234305221c56faf4657c34e6
Amezande <oscarabrjs@gmail.com>
2026-03-25 13:54:17 -0600

    Add unit tests for EMTU module functions

---
+++ Unit_Testing_Modules/Test_EMTU.py
@@ -0,0 +1,13 @@
+#Testing Example Module Testing Unit
+import unittest
+
+from EMTU import square, doubler
+class TestMyModule(unittest.TestCase):
+  def test_square(self):
+    self.assertEqual(square(2), 4)
+
+  def test_doubler(self):
+    self.assertEqual(doubler(0), 0)
+
+if __name__ == '__main__':
+  unittest.main()
