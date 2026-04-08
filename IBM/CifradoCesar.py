e1f8034bf86d8dd41069b64a60aede3eb62bd672
Amezande <oscarabrjs@gmail.com>
2026-04-07 21:02:54 -0600

    Refactor CifradoCesar for improved functionality

--- IBM/CifradoCesar.py
+++ IBM/CifradoCesar.py
@@ -1,34 +1,50 @@
+import string
 alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+caracteres = [" ",",",".",":"]
 
 def cifrado_cesar(texto, desplazamiento):
     i = 0
     iter = 0
-    #Variable para el texto cifrado
-    texto_cifrado = " "
+    texto_cifrado = ""
 
-    
     for caracter in texto :
-        #Imprime hola
-        #print(caracter)
         
         if caracter in alfabeto:
-            #print(f"Contiene {caracter}")
-            #print(alfabeto.index(caracter))
             nvoindice = alfabeto.index(caracter) + desplazamiento
-            print(alfabeto[nvoindice])
-        #i += desplazamiento
-        iter += 1
-        
-    if caracter not in alfabeto :
-        print("Tu caracter no está en el alfabeto")
+            nvoindice = nvoindice % len(alfabeto)
+            texto_cifrado += alfabeto[nvoindice]
+        elif caracter in caracteres:
+            texto_cifrado += caracter
+            #print(caracter)
+        elif caracter.islower():
+            try :
+                raise ValueError 
+            except ValueError:
+                #print("")
+                return "Todos los caracteres deben estar en mayúsculas y dentro del alfabeto."
+                
+        else :
+            texto_cifrado += caracter
+    #iter += 1
+    return texto_cifrado
     
+def descifrado_cesar(texto_cifrado, deslpazamiento):
+    texto_descifrado = ""
+
+    for caracter in texto_cifrado:
+        if caracter in alfabeto:
+            nvoindice1 = alfabeto.index(caracter) - deslpazamiento
+            nvoindice1 = nvoindice1 % len(alfabeto)
+            texto_descifrado += alfabeto[nvoindice1]
+        elif caracter in caracteres :
+            texto_descifrado += caracter
+        else : 
+            texto_descifrado += caracter
+    return texto_descifrado
     
-    #for i in range(len(alfabeto)) :
-        #Imprime el alfabeto
-        #print(alfabeto[i])
 
-    #i+= 1
-    #print(texto)
-    #print(desplazamiento)
 
-a = cifrado_cesar("HOLA MUNDO",1)
+a = cifrado_cesar("hola mundo.",1)
+print(a)
+b = descifrado_cesar(a,1)
+print(b)
